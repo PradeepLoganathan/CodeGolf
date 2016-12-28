a7a7ec0fd7203e2740d6efba54a1fcbeef243445
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2016-12-28 15:57:49 +0530

    Added tests with MStest runner.

--- CodeGolf.Test/StringandArraysTest.cs
+++ CodeGolf.Test/StringandArraysTest.cs
@@ -1,19 +1,53 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CodeGolf;
+using System.Diagnostics;
 
 namespace CodeGolf.Test
 {
     [TestClass]
     public class StringandArraysTest
     {
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestCheckUniqueString()
         {
             StringsandArrays SnA = new StringsandArrays();
-            SnA.CheckUniqueString("abcdefghi");
-            SnA.Urlifyastring("This is    a  string that   has to    be urilfyied", 1);
-            SnA.CheckPermutation("Hello", "oelHl");
+            Assert.IsTrue(SnA.CheckUniqueString("abcdefghi"));
+            Assert.IsFalse(SnA.CheckUniqueString("Hello"));
         }
+
+        [TestMethod]
+        public void UrlifyastringTest()
+        {
+            StringsandArrays SnA = new StringsandArrays();
+            string urlified;
+            urlified = SnA.Urlifyastring("This is    a  string that   has to    be urilfyied", 1);
+            Assert.IsTrue(urlified.Equals("This%20is%20a%20string%20that%20has%20to%20be%20urilfyied"));
+        }
+
+        [TestMethod]
+        public void CheckPermutationTest()
+        {
+            StringsandArrays SnA = new StringsandArrays();
+            Assert.IsTrue(SnA.CheckPermutation("Hello", "oelHl"));
+
+        }
+
+        [TestMethod]
+        public void CheckPermutationOfPalindromeTest()
+        {
+            StringsandArrays SnA = new StringsandArrays();
+            string[] palindromes;
+
+            Assert.IsTrue(SnA.CheckPermutationOfPalindrome("Tenet", out palindromes));
+            Debug.WriteLine(String.Join(", ", palindromes));
+            
+            
+        }
+
+
+
     }
 }
